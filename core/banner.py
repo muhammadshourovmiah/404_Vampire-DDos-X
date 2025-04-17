796b0267eb2df6f29a86d29ad26f5186640c0086
Muhammad Shourov (Vampire) <114272144+muhammadshourovmiah@users.noreply.github.com>
2025-04-17 14:05:05 +0600

    Update banner.py

--- core/banner.py
+++ core/banner.py
@@ -1,1 +1,32 @@
+from pystyle import Colors, Colorate
 
+def admin_banner():
+    banner = f"""
+{Colors.red}
+███████╗ █████╗ ███╗   ███╗██████╗ ██╗███╗   ███╗███████╗
+██╔════╝██╔══██╗████╗ ████║██╔══██╗██║████╗ ████║██╔════╝
+█████╗  ███████║██╔████╔██║██████╔╝██║██╔████╔██║█████╗  
+██╔══╝  ██╔══██║██║╚██╔╝██║██╔═══╝ ██║██║╚██╔╝██║██╔══╝  
+███████╗██║  ██║██║ ╚═╝ ██║██║     ██║██║ ╚═╝ ██║███████╗
+╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝     ╚═╝╚═╝     ╚═╝╚══════╝
+
+            {Colors.green}==> Vampire-Admin-Access
+         Coded by Muhammad Shourov (VAMPIRE)
+"""
+    return Colorate.Horizontal(Colors.red_to_yellow, banner, 2)
+
+def user_banner():
+    banner = f"""
+{Colors.blue}
+██╗   ██╗███████╗███████╗██████╗     ██╗   ██╗███████╗
+██║   ██║██╔════╝██╔════╝██╔══██╗    ██║   ██║██╔════╝
+██║   ██║█████╗  █████╗  ██████╔╝    ██║   ██║█████╗  
+██║   ██║██╔══╝  ██╔══╝  ██╔═══╝     ╚██╗ ██╔╝██╔══╝  
+╚██████╔╝███████╗███████╗██║          ╚████╔╝ ███████╗
+ ╚═════╝ ╚══════╝╚══════╝╚═╝           ╚═══╝  ╚══════╝
+
+        {Colors.green}==> Free-User-Access
+      Author: Muhammad Shourov
+      GitHub: https://github.com/vampiresquad
+"""
+    return Colorate.Horizontal(Colors.blue_to_cyan, banner, 2)
