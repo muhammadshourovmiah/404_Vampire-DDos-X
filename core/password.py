8ec5d94718bde96681ab6ec57bf7ea6542960de9
Muhammad Shourov (Vampire) <114272144+muhammadshourovmiah@users.noreply.github.com>
2025-04-17 14:05:57 +0600

    Update password.py

--- core/password.py
+++ core/password.py
@@ -1,1 +1,8 @@
-
+def check_password():
+    correct_password = "SH404"
+    try:
+        user_input = input("Enter Admin Password: ")
+        return user_input == correct_password
+    except KeyboardInterrupt:
+        print("\nAborted.")
+        return False
